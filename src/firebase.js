16196cf496b9d973a7d51ae8dd98ba27d8736dfc
Tusshar-developer <tussharspinner@gmail.com>
2022-09-02 23:24:31 +1000

    dotenv testing

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const firebaseConfig = {
   apiKey: process.env.apiKey,

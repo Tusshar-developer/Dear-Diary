7431704ad54405e93f0f2f08e6639eb2fe64d541
Tusshar-developer <tussharspinner@gmail.com>
2022-09-03 22:19:59 +1000

    Deleted process.env console log in src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -14,8 +14,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_appId,
 };
 
-console.log(process.env);
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
